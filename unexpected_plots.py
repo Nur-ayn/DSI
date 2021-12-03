77862db38b9096cb0cd3aedf0b57a3106b3d4653
Nur-ayn <nuraynhashim@outlook.com>
2021-12-03 12:14:33 +0200

    implementing unexpected_plot function and helper functions for Q1

---
+++ unexpected_plots.py
@@ -0,0 +1,49 @@
+# QUESTION 1 - Unexpected Plot
+
+import matplotlib.pyplot as plt
+import numpy as np
+from shapely.geometry import Polygon, Point
+import random
+
+def random_point_within(polygon):
+    min_x, min_y, max_x, max_y = polygon.bounds
+    point = None
+
+    while point is None:
+        random_pt = Point([random.uniform(min_x, max_x), random.uniform(min_y, max_y)])
+        if random_pt.within(polygon):
+            point = random_pt
+
+    return point
+
+def get_triangle_centroid(vertices):
+    vertices = np.array(vertices)
+    xs, ys = vertices[:,0], vertices[:,1]
+
+    centroid = [np.sum(xs)/3, np.sum(ys)/3]
+    return centroid
+
+def unexpected_plot():
+    hexagon_vertices = [ (-3,0), (-1.5, 1.5), (1.5, 1.5), (3, 0), (1.5, -1.5), (-1.5, -1.5)]
+    hexagon = Polygon(hexagon_vertices)
+    plt.plot(*hexagon.exterior.xy)
+
+    P = random_point_within(hexagon)
+
+    for _ in range(10):
+        plt.plot(P.x, P.y, marker='o')
+        random_vertex_index = random.randint(0,5)
+        adjacent_vertices = [hexagon_vertices[random_vertex_index], hexagon_vertices[(random_vertex_index+1)%6]]
+
+        triangle_vertices = adjacent_vertices + [(P.x, P.y)]
+        triangle = Polygon(triangle_vertices)
+        plt.plot(*triangle.exterior.xy)
+
+        P = Point(get_triangle_centroid(triangle_vertices))
+
+
+if __name__ == '__main__':
+    unexpected_plot()
+    plt.show()
+   
+    
